1b4a83e418d7a364b7cbc8fb42488fb7ef76f5c7
Scarlet Saavedra <scar.saavedrar@gmail.com>
2025-02-25 22:54:09 -0500

    mouvement up down reussi

---
+++ Assets/InPut/PlayerMouvement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMouvement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float moveSpeed;
+    private Vector3 _moveDirection;
+
+    public InputActionReference move;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector3>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+}
