e7c87a944c0f41b7176143c2d05ad7ce46c6dda2
Scarlet Saavedra <scar.saavedrar@gmail.com>
2025-02-25 21:00:20 -0500

    input refait

---
+++ Assets/Scripts/SousMarin.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SousMarin : MonoBehaviour
+{
+
+
+// varaibles de mouvement et contr�le
+    [SerializeField] private float vitessePromenade;
+    private Rigidbody _rb;
+    private Vector3 directionInput;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue directionBase)
+    {
+        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * vitessePromenade;
+        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
+    }
+
+    void FixedUpdate()
+    {
+        // calculer et appliquer la translation
+        Vector3 mouvement = directionInput;
+
+        // si on a une direction d'input
+        if (directionInput.magnitude > 0f)
+        {
+            // calculer rotation cible
+            float rotationCible = Vector3.SignedAngle(Vector3.forward, directionInput.normalized , Vector3.up);
+            // appliquer la rotation cible directement
+            _rb.MoveRotation(Quaternion.Euler(0.0f, rotationCible, 0.0f));
+            _rb.AddRelativeForce(0, 0, mouvement.magnitude, ForceMode.VelocityChange);
+        }
+    }
+
+
+    
+}
